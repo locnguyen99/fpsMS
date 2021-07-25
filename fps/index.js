a9219b587e69ebf00709f98086a0f8b02c6376b0
phuminh <phuminh@maysoft.vn>
2021-07-25 09:32:36 +0700

    layout

--- fps/index.js
+++ fps/index.js
@@ -1,10 +1,12 @@
 $(document).ready( function() {
-    $('.container-fluid').first().removeClass('container-fluid').addClass('container');
+    $('.container-fluid').first().removeClass('container-fluid').addClass('fps-container');
     $('#main').parent().removeClass('row');
     $('.breadcrumbsWrapper').parent().addClass('container-fluid ');
     $('.navbar').parent().addClass('no-padding-left');
     $('.footer hr').remove();
     $('.logo-footer').parent().removeClass('container');
+
+    $('#main .container-fluid').removeClass('container-fluid');
 
     // load logo
     $('#sidebar-wrapper a:first').replaceWith(logo);
