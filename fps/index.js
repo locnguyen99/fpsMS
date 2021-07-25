38c5a01b8e91ec7ea8de3ef85bd8c33fb938397f
locnguyen <iot.loc.nguyen@gmail.com>
2021-07-25 19:00:56 +0700

    update code

--- fps/index.js
+++ fps/index.js
@@ -13,11 +13,16 @@ $(document).ready( function() {
     $('.breadcrumbsWrapper .row:first').removeClass('row').addClass('flex-space-between');
     $('.flex-space-between .col-md-6:first').removeClass('col-md-6');
     $('.flex-space-between .col-md-6:first').removeClass('col-md-6');
+    //notification pgae
+    $('.flex-space-between .col-md-10:first').removeClass('col-md-10');
+    $('.flex-space-between .col-md-2:first').removeClass('col-md-2');
 
 
     // load logo
     $('#sidebar-wrapper a:first').replaceWith(logo);
     // load left menu
     $('ul.sidebar-nav.list-unstyled').replaceWith(menu);
+
+    $(".toggle.btn.btn-default.off.btn-sm.fps-toggle").children().first().remove();
 });
 
