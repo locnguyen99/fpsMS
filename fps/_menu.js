1b33a147b49c4e3adc8fa9cd52694e1d54b5f935
locnguyen <iot.loc.nguyen@gmail.com>
2021-07-26 05:57:18 +0700

    edit menu.js

--- fps/_menu.js
+++ fps/_menu.js
@@ -1,3 +1,4 @@
+
 var menu = `
 <ul class="sidebar-nav list-unstyled" style="margin-top:30px;padding-left:20px;">
   <li class="sidebar-brand menuliparent">
@@ -26,7 +27,7 @@ var menu = `
         aria-hidden="true"></i> <span class="hidetitle">Beheer </span></a>
   </li>
   <li class="menuliparent">
-    <a href="_MeldingenOverzicht.html"><i class="fa fa-asterisk fa-lg"
+    <a href="_Meldingenoverzicht.html"><i class="fa fa-asterisk fa-lg"
         aria-hidden="true"></i> <span class="hidetitle">Melding overzicht </span></a>
   </li>
   <li class="menuliparent">
@@ -44,14 +45,8 @@ var menu = `
       <li class="list-group-item"><a
           href="_GebruikersOverzicht.html">Gebruiker</a></li>
       <li class="list-group-item"><a
-          href="_Locatiesoverzicht.html">Locatie</a></li>
+          href="_LocatiesOverzicht.html">Locatie</a></li>
     </ul>
   </li>
 </ul>
-<div class="my-controls">
-  <label class="chk-container">
-    <input type="checkbox">
-    <span class="checkmark"></span>
-  </label>
-</div>
 `
